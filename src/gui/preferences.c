5be92afea13662b5eb775837af85070ab95ef500
Christian Kr <cerebrosus@gmx.net>
2024-11-24 10:14:29 +0100

    Migrate _gui_preferences_int_reset to GtkGesture.

--- src/gui/preferences.c
+++ src/gui/preferences.c
@@ -1244,9 +1244,14 @@ void dt_gui_preferences_int_reset(GtkWidget *widget)
 }
 
 static gboolean
-_gui_preferences_int_reset(GtkWidget *label, GdkEventButton *event, GtkWidget *widget)
+_gui_preferences_int_reset(
+    GtkGestureMultiPress *gesture,
+    int n_press,
+    double x,
+    double y,
+    GtkWidget *widget)
 {
-  if(event->type == GDK_2BUTTON_PRESS)
+  if (n_press == 2)
   {
     dt_gui_preferences_int_reset(widget);
     return TRUE;
@@ -1279,7 +1284,14 @@ GtkWidget *dt_gui_preferences_int(GtkGrid *grid, const char *key, const guint co
   gtk_grid_attach(GTK_GRID(grid), labelev, col, line, 1, 1);
   gtk_grid_attach(GTK_GRID(grid), w, col + 1, line, 1, 1);
   g_signal_connect(G_OBJECT(w), "value-changed", G_CALLBACK(_gui_preferences_int_callback), (gpointer)key);
-  g_signal_connect(G_OBJECT(labelev), "button-press-event", G_CALLBACK(_gui_preferences_int_reset), (gpointer)w);
+
+  dtgtk_button_default_handler_new(
+    GTK_WIDGET(labelev),
+    GDK_BUTTON_PRIMARY,
+    G_CALLBACK(_gui_preferences_int_reset),
+    NULL,
+    w);
+
   return w;
 }
 
